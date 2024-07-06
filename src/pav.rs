adaa3d6c59159bfa453992d24e773e25ad4fd897
Ian Clarke <github@ian.33mail.com>
2024-07-06 13:12:24 -0400

    Updated import path for IsotonicRegression in pav.rs.

--- src/pav.rs
+++ src/pav.rs
@@ -4,4 +4,4 @@ mod isotonic_regression;
 
 pub use coordinate::Coordinate;
 pub use point::Point;
-pub use isotonic_regression::{IsotonicRegression, IsotonicRegressionError};
+pub use self::isotonic_regression::{IsotonicRegression, IsotonicRegressionError};
